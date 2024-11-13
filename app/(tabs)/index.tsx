a5ad880b854b61b41be76838745160f6a689a25f
Nathan Smith <nathanielvs@gmail.com>
2024-11-13 11:16:37 -0700

    Pick an image from the device's media library; import ImagePicker and add async function to allow user to pick image. Update the Button component; add onPress as a function prop.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import { View, StyleSheet } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
 import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
@@ -6,13 +7,31 @@ import ImageViewer from "@/components/ImageViewer";
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
+    const pickImageAsync = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            console.log(result);
+        } else {
+            alert("You did not select any image.");
+        }
+    };
+
     return (
         <View style={styles.container}>
             <View style={styles.imageContainer}>
                 <ImageViewer imgSource={PlaceholderImage} />
             </View>
             <View style={styles.footerContainer}>
-                <Button theme="primary" label="Choose a photo" />
+                <Button
+                    theme="primary"
+                    label="Choose a photo"
+                    onPress={pickImageAsync}
+                />
                 <Button label="Use this photo" />
             </View>
         </View>
@@ -27,7 +46,6 @@ const styles = StyleSheet.create({
     },
     imageContainer: {
         flex: 1,
-        paddingTop: 28,
     },
     footerContainer: {
         flex: 1 / 3,
