355d8a568dceed9ccbce33d9f4e27138ef8b28b8
Nathan Smith <nathanielvs@gmail.com>
2024-11-13 09:43:55 -0700

    Create buttons using Pressable. Add Button component. Import Button into Home.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,6 @@
 import { View, StyleSheet } from "react-native";
+
+import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
@@ -8,6 +10,10 @@ export default function Index() {
         <View style={styles.container}>
             <View style={styles.imageContainer}>
                 <ImageViewer imgSource={PlaceholderImage} />
+            </View>
+            <View style={styles.footerContainer}>
+                <Button label="Choose a photo" />
+                <Button label="Use this photo" />
             </View>
         </View>
     );
@@ -21,5 +27,10 @@ const styles = StyleSheet.create({
     },
     imageContainer: {
         flex: 1,
+        paddingTop: 28,
+    },
+    footerContainer: {
+        flex: 1 / 3,
+        alignItems: "center",
     },
 });
