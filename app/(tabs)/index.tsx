81d5d41b81c0ba587c3dbd22078b2f3b1b8317f8
Nathan Smith <nathanielvs@gmail.com>
2024-11-14 10:55:14 -0700

    Import CircleButton and IconButton components and add placeholder functions for each button.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 
 import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
+import IconButton from "@/components/IconButton";
+import CircleButton from "@/components/CircleButton";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
@@ -28,6 +30,18 @@ export default function Index() {
         }
     };
 
+    const onReset = () => {
+        setShowAppOptions(false);
+    };
+
+    const onAddSticker = () => {
+        // we will implement this later
+    };
+
+    const onSaveImageAsync = async () => {
+        // we will implement this later
+    };
+
     return (
         <View style={styles.container}>
             <View style={styles.imageContainer}>
@@ -37,7 +51,21 @@ export default function Index() {
                 />
             </View>
             {showAppOptions ? (
-                <View />
+                <View style={styles.optionsContainer}>
+                    <View style={styles.optionsRow}>
+                        <IconButton
+                            icon="refresh"
+                            label="Reset"
+                            onPress={onReset}
+                        />
+                        <CircleButton onPress={onAddSticker} />
+                        <IconButton
+                            icon="save-alt"
+                            label="Save"
+                            onPress={onSaveImageAsync}
+                        />
+                    </View>
+                </View>
             ) : (
                 <View style={styles.footerContainer}>
                     <Button
@@ -67,5 +95,13 @@ const styles = StyleSheet.create({
     footerContainer: {
         flex: 1 / 3,
         alignItems: "center",
+    },
+    optionsContainer: {
+        position: "absolute",
+        bottom: 80,
+    },
+    optionsRow: {
+        alignItems: "center",
+        flexDirection: "row",
     },
 });
