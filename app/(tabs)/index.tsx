6ccab3d4721138a64c983421ff7501ee8102fb4e
Nathan Smith <nathanielvs@gmail.com>
2024-11-15 19:40:58 -0700

    Display a list of emoji; create an EmojiList component, import into Home, and add useState for pickedEmoji

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,14 @@
 import { View, StyleSheet } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
+import { type ImageSource } from "expo-image";
 
 import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
 import IconButton from "@/components/IconButton";
 import CircleButton from "@/components/CircleButton";
 import EmojiPicker from "@/components/EmojiPicker";
+import EmojiList from "@/components/EmojiList";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
@@ -16,6 +18,9 @@ export default function Index() {
     );
     const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
     const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+    const [pickedEmoji, setPickedEmoji] = useState<ImageSource | undefined>(
+        undefined
+    );
 
     const pickImageAsync = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
@@ -86,7 +91,10 @@ export default function Index() {
                 </View>
             )}
             <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
-                {/* A list of emoji component will go here */}
+                <EmojiList
+                    onSelect={setPickedEmoji}
+                    onCloseModal={onModalClose}
+                />
             </EmojiPicker>
         </View>
     );
