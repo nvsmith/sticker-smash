94df761d7d95321c381521e444b4250f40710e77
Nathan Smith <nathanielvs@gmail.com>
2024-11-15 20:15:49 -0700

    Add GestureHandlerRootView; get gesture interactions to work in the app.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import { View, StyleSheet } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
 import { type ImageSource } from "expo-image";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
@@ -55,7 +56,7 @@ export default function Index() {
     };
 
     return (
-        <View style={styles.container}>
+        <GestureHandlerRootView style={styles.container}>
             <View style={styles.imageContainer}>
                 <ImageViewer
                     imgSource={PlaceholderImage}
@@ -100,7 +101,7 @@ export default function Index() {
                     onCloseModal={onModalClose}
                 />
             </EmojiPicker>
-        </View>
+        </GestureHandlerRootView>
     );
 }
 
