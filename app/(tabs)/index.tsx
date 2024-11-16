95a471066a70af352ccb2778af75ac7e2bc95171
Nathan Smith <nathanielvs@gmail.com>
2024-11-15 20:01:00 -0700

    Display the selected emoji; create a new EmoiSticker component, import it into Home, and conditionally render an emoji sticker if picked.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,6 +9,7 @@ import IconButton from "@/components/IconButton";
 import CircleButton from "@/components/CircleButton";
 import EmojiPicker from "@/components/EmojiPicker";
 import EmojiList from "@/components/EmojiList";
+import EmojiSticker from "@/components/EmojiSticker";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
@@ -60,6 +61,9 @@ export default function Index() {
                     imgSource={PlaceholderImage}
                     selectedImage={selectedImage}
                 />
+                {pickedEmoji && (
+                    <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+                )}
             </View>
             {showAppOptions ? (
                 <View style={styles.optionsContainer}>
