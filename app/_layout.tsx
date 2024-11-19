1a153841d37e0ae10ab2b4dbf860b54a010e4327
Nathan Smith <nathanielvs@gmail.com>
2024-11-18 20:53:25 -0700

    Configure the status bar; set status bar style to light when app loads.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,18 @@
 import { Stack } from "expo-router";
 
+import { setStatusBarStyle } from "expo-status-bar";
+import { useEffect } from "react";
+
 export default function RootLayout() {
+    useEffect(() => {
+        setTimeout(() => {
+            setStatusBarStyle("light");
+        }, 0);
+    }, []); // Empty array means "run only once"
+
     return (
         <Stack>
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="+not-found" />
         </Stack>
     );
 }
